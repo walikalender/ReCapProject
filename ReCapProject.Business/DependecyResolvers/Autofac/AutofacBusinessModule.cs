c5b6ef7c7603e45e12fac55b0f2dd65e2543546c
ali kalender <waalinizz@gmail.com>
2023-10-08 13:31:29 +0300

    autofacbusinessmodule added. program.cs did configure builder

---
+++ ReCapProject.Business/DependecyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using ReCapProject.Business.Abstract;
+using ReCapProject.Business.Concrete;
+using ReCapProject.DataAccess.Abstract;
+using ReCapProject.DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReCapProject.Business.DependecyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CarManager>().As<ICarService>();
+            builder.RegisterType<EfCarDal>().As<ICarDal>();
+
+            builder.RegisterType<BrandManager>().As<IBrandService>();
+            builder.RegisterType<EfBrandDal>().As<IBrandDal>();
+
+            builder.RegisterType<ColorManager>().As<IColorService>();
+            builder.RegisterType<EfColorDal>().As<IColorDal>();
+
+            builder.RegisterType<CustomerManager>().As<ICustomerService>();
+            builder.RegisterType<EfCustomerDal>().As<ICustomerDal>();
+
+            builder.RegisterType<RentalManager>().As<IRentalService>();
+            builder.RegisterType<EfRentalDal>().As<IRentalDal>();
+
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<EfUserDal>().As<IUserDal>();
+        }
+    }
+}
